0cae7c38d4fd86278a00942150499a0bcd5ec811
Nightburnn <nightburn2003@gmail.com>
2023-05-05 16:50:31 +0100

    start Flask web application

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,7 +7,11 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
+from uuid import uuid4
+from flasgger import Swagger
+swagger = Swagger(app)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -18,7 +22,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -37,7 +41,7 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
+                           places=places, cache_id=str(uuid4()))
 
 
 if __name__ == "__main__":
