c9dbf219a5def0ba8b102c4c7b91a1b8f0297484
isaaccleopas <isaaccleopas@gmail.com>
2023-05-06 13:48:13 +0000

    header.css

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
       $("DIV#api_status").removeClass("available");
     }
   });
+
+  const amenities = {};
+    $("li input[type=checkbox]").change(function () {
+      if (this.checked) {
+        amenities[this.dataset.name] = this.dataset.id;
+      } else {
+        delete amenities[this.dataset.name];
+      }
+        $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+  });
 });
